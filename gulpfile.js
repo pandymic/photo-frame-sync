c102ebe6e15f0d17fef592a150c22a70cd55e5b1
Michal Pandyra <mike@pandymic.com>
2024-02-24 11:36:07 -0800

    Updated JS and CSS dev environment.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,44 @@
 const gulp = require( 'gulp' ),
   log = require( 'fancy-log' ),
-  shell = require( 'child_process' ).exec;
+  plumber = require('gulp-plumber'),
+  sass = require('gulp-sass')( require('sass') ),
+  shell = require( 'child_process' ).exec,
+  uglify = require('gulp-uglify');
 
 gulp.task( 'dist', () => {
   return gulp.src( [
     './src/**/*',
     '!./src/photo-frame/sync',
+    '!./src/photo-frame/script.js',
+    '!./src/photo-frame/style.scss',
     '!./src/**/*.md',
     '!./src/**/.gitignore'
   ] )
     .pipe( gulp.dest( './dist' ) );
 } );
+
+gulp.task( 'style', () => {
+  return gulp.src( './src/photo-frame/style.scss' )
+    .pipe( plumber() )
+    .pipe( sass( {
+      outputStyle: 'compressed',
+      functions: {
+        // Base64 encode strings for data url's within sass files.
+        'btoa($string)': function(string) {
+          string.setValue( Buffer.from( string.getValue() ).toString( 'base64' ) );
+          return string;
+        }
+      }
+    } ) )
+    .pipe( gulp.dest( './dist/photo-frame' ) );
+});
+
+gulp.task( 'script', () => {
+  return gulp.src( './src/photo-frame/script.js' )
+    .pipe( plumber() )
+    .pipe( uglify() )
+    .pipe( gulp.dest( './dist/photo-frame' ) );
+});
 
 gulp.task( 'permissions', ( callback ) => {
   shell( 'cd ./dist && find . -user $USER -not -group apache -exec chgrp apache {} \\;', ( err, stdout, stderr ) => {
@@ -31,4 +59,4 @@ gulp.task( 'watch', () => {
   gulp.watch( './src/**/*', gulp.series( 'default' ) );
 } );
 
-gulp.task( 'default', gulp.series( 'dist', 'permissions' ) );
+gulp.task( 'default', gulp.series( 'dist', 'style', 'script', 'permissions' ) );
